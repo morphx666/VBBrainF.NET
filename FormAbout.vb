ebba691402a07b224dd0f27e3b240581644a6c82
Xavier Flix <xavier@xfx.net>
2019-11-10 02:08:09 -0400

    Updated About dialog's links

--- FormAbout.vb
+++ FormAbout.vb
@@ -37,6 +37,7 @@ Public Class FormAbout
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Panel2 As System.Windows.Forms.Panel
     Friend WithEvents LinkBF As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkGitHub As LinkLabel
     Friend WithEvents LinkOWS As System.Windows.Forms.LinkLabel
     <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormAbout))
@@ -49,6 +50,7 @@ Public Class FormAbout
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.LinkBF = New System.Windows.Forms.LinkLabel()
         Me.LinkOWS = New System.Windows.Forms.LinkLabel()
+        Me.LinkGitHub = New System.Windows.Forms.LinkLabel()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -60,7 +62,7 @@ Public Class FormAbout
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(417, 49)
+        Me.Panel1.Size = New System.Drawing.Size(581, 49)
         Me.Panel1.TabIndex = 0
         '
         'Label2
@@ -88,7 +90,7 @@ Public Class FormAbout
         '
         Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button1.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Button1.Location = New System.Drawing.Point(344, 280)
+        Me.Button1.Location = New System.Drawing.Point(508, 331)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(68, 30)
         Me.Button1.TabIndex = 1
@@ -98,13 +100,15 @@ Public Class FormAbout
         '
         Me.LinkXFX.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.LinkXFX.LinkArea = New System.Windows.Forms.LinkArea(22, 45)
-        Me.LinkXFX.Location = New System.Drawing.Point(14, 232)
+        Me.LinkXFX.AutoSize = True
+        Me.LinkXFX.LinkArea = New System.Windows.Forms.LinkArea(22, 98)
+        Me.LinkXFX.Location = New System.Drawing.Point(14, 238)
         Me.LinkXFX.Name = "LinkXFX"
-        Me.LinkXFX.Size = New System.Drawing.Size(391, 16)
+        Me.LinkXFX.Size = New System.Drawing.Size(561, 37)
         Me.LinkXFX.TabIndex = 2
         Me.LinkXFX.TabStop = True
-        Me.LinkXFX.Text = "VBBrainFNET Web Site: http://software.xfx.net/utilities/vbbfck/"
+        Me.LinkXFX.Text = "VBBrainFNET Web Site: " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "https://whenimbored.xfx.net/2013/01/brainfuck-compiler-in" &
+    "terpreter-and-debugger-written-in-vb-net"
         Me.LinkXFX.UseCompatibleTextRendering = True
         Me.LinkXFX.UseMnemonic = False
         '
@@ -114,7 +118,7 @@ Public Class FormAbout
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label3.Location = New System.Drawing.Point(14, 62)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(401, 66)
+        Me.Label3.Size = New System.Drawing.Size(565, 66)
         Me.Label3.TabIndex = 3
         Me.Label3.Text = resources.GetString("Label3.Text")
         '
@@ -123,23 +127,24 @@ Public Class FormAbout
         Me.Panel2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Panel2.Location = New System.Drawing.Point(-11, 138)
+        Me.Panel2.Location = New System.Drawing.Point(-11, 132)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(435, 4)
+        Me.Panel2.Size = New System.Drawing.Size(599, 4)
         Me.Panel2.TabIndex = 4
         '
         'LinkBF
         '
         Me.LinkBF.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LinkBF.AutoSize = True
         Me.LinkBF.LinkArea = New System.Windows.Forms.LinkArea(83, 122)
         Me.LinkBF.Location = New System.Drawing.Point(14, 154)
         Me.LinkBF.Name = "LinkBF"
-        Me.LinkBF.Size = New System.Drawing.Size(379, 29)
+        Me.LinkBF.Size = New System.Drawing.Size(435, 37)
         Me.LinkBF.TabIndex = 5
         Me.LinkBF.TabStop = True
         Me.LinkBF.Text = "Brainfuck is a creation of Urban Müller. Follow this link for further information" &
-    ": http://en.wikipedia.org/wiki/Brainfuck"
+    ":" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "http://en.wikipedia.org/wiki/Brainfuck"
         Me.LinkBF.UseCompatibleTextRendering = True
         Me.LinkBF.UseMnemonic = False
         '
@@ -147,23 +152,40 @@ Public Class FormAbout
         '
         Me.LinkOWS.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LinkOWS.AutoSize = True
         Me.LinkOWS.LinkArea = New System.Windows.Forms.LinkArea(38, 77)
-        Me.LinkOWS.Location = New System.Drawing.Point(14, 193)
+        Me.LinkOWS.Location = New System.Drawing.Point(14, 196)
         Me.LinkOWS.Name = "LinkOWS"
-        Me.LinkOWS.Size = New System.Drawing.Size(379, 29)
+        Me.LinkOWS.Size = New System.Drawing.Size(246, 37)
         Me.LinkOWS.TabIndex = 6
         Me.LinkOWS.TabStop = True
-        Me.LinkOWS.Text = "(almost) Official Brainfuck Web Site: http://www.muppetlabs.com/~breadbox/bf/"
+        Me.LinkOWS.Text = "(almost) Official Brainfuck Web Site:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "http://www.muppetlabs.com/~breadbox/bf/"
         Me.LinkOWS.UseCompatibleTextRendering = True
+        '
+        'LinkGitHub
+        '
+        Me.LinkGitHub.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LinkGitHub.AutoSize = True
+        Me.LinkGitHub.LinkArea = New System.Windows.Forms.LinkArea(20, 41)
+        Me.LinkGitHub.Location = New System.Drawing.Point(17, 280)
+        Me.LinkGitHub.Name = "LinkGitHub"
+        Me.LinkGitHub.Size = New System.Drawing.Size(252, 37)
+        Me.LinkGitHub.TabIndex = 2
+        Me.LinkGitHub.TabStop = True
+        Me.LinkGitHub.Text = "GitHub Repository:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "https://github.com/morphx666/VBBrainF.NET"
+        Me.LinkGitHub.UseCompatibleTextRendering = True
+        Me.LinkGitHub.UseMnemonic = False
         '
         'FormAbout
         '
         Me.AutoScaleBaseSize = New System.Drawing.Size(6, 16)
-        Me.ClientSize = New System.Drawing.Size(417, 315)
+        Me.ClientSize = New System.Drawing.Size(581, 366)
         Me.Controls.Add(Me.LinkOWS)
         Me.Controls.Add(Me.LinkBF)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LinkGitHub)
         Me.Controls.Add(Me.LinkXFX)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Panel1)
@@ -178,26 +200,33 @@ Public Class FormAbout
         Me.Text = "About VBBrainFNET"
         Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
 #End Region
 
-    Private Sub LinkBF_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkBF.LinkClicked
+    Private Sub LinkBF_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkBF.LinkClicked
         With LinkBF
-            System.Diagnostics.Process.Start(.Text.Substring(.LinkArea.Start))
+            Process.Start(.Text.Substring(.LinkArea.Start))
         End With
     End Sub
 
-    Private Sub LinkXFX_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkXFX.LinkClicked
+    Private Sub LinkXFX_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkXFX.LinkClicked
         With LinkXFX
-            System.Diagnostics.Process.Start(.Text.Substring(.LinkArea.Start))
+            Process.Start(.Text.Substring(.LinkArea.Start))
         End With
     End Sub
 
-    Private Sub LinkOWS_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkOWS.LinkClicked
+    Private Sub LinkOWS_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkOWS.LinkClicked
         With LinkOWS
-            System.Diagnostics.Process.Start(.Text.Substring(.LinkArea.Start))
+            Process.Start(.Text.Substring(.LinkArea.Start))
+        End With
+    End Sub
+
+    Private Sub LinkGitHub_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkGitHub.LinkClicked
+        With LinkGitHub
+            Process.Start(.Text.Substring(.LinkArea.Start))
         End With
     End Sub
 End Class
